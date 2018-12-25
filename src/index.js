ecdb066b534476505aa5502091838daa9b11e524
moody <dethanyel@gmail.com>
2018-12-25 11:47:44 -0500

    rem fcc tests

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-// Import FCC Test Suite
-const fcc = document.createElement('script');
-fcc.src = 'https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js';
-document.head.appendChild(fcc);
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
